9f971898a2eb838205842db9e849c97b1753b2fc
BillStephens2022 <two4onebill@yahoo.com>
2023-12-03 11:27:03 -0500

    updated dependency versions to fix errors with starting app, added shell components for screens  and Primary Button

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View, TextInput } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen() {
+  return (
+    <View>
+        <TextInput />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  )
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+
+});
