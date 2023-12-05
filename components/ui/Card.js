7198c645c99887dec67579f03f9c9cee9cba148d
BillStephens2022 <two4onebill@yahoo.com>
2023-12-05 07:08:41 -0500

    adjusted some styling to be responsive to smaller screen sizes

--- components/ui/Card.js
+++ components/ui/Card.js
@@ -1,17 +1,19 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 function Card({ children }) {
-  return <View style={styles.inputContainer}>{children}</View>;
+  return <View style={styles.card}>{children}</View>;
 }
 
 export default Card;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
-  inputContainer: {
+  card: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 36,
+    marginTop: deviceWidth < 380 ? 18 : 36,
     marginHorizontal: 24,
     padding: 16,
     backgroundColor: Colors.primary800,
